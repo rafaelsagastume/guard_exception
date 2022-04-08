8eff055ffd77e31ec95c5172e0e01e72793eff3e
Rafael Garcia Sagastume <rafael.garcia@ciberc.com>
2022-04-07 19:20:40 -0600

    transform function to class for fix import module

--- guard_exception/__init__.py
+++ guard_exception/__init__.py
@@ -1,27 +1,31 @@
 __autor__ = "Rafael Fernando Garcia Sagastume"
 __copyright__ = "Copyright 2022, Rafael Fernando Garcia Sagastume"
 __license__ = "BSD 3-Clause License"
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 __maintainer__ = "Rafael Fernando Garcia Sagastume"
 __status__ = "Production"
 
 
-def guardException(f, **kwargs):
-    """
-    Description: catch fatal errors or any type of error,
-    used to easily normalize executions in fast tween calls with PythonKit
+class guard_exception:
+    def __init__(self):
+        pass
 
-    arguments: f: function to be executed
-    kwargs: arguments to be passed to the function
+    def guardException(f, **kwargs):
+        """
+        Description: catch fatal errors or any type of error,
+        used to easily normalize executions in fast tween calls with PythonKit
 
-    return: result of the function or str error message if any error occurs
-    """
+        arguments: f: function to be executed
+        kwargs: arguments to be passed to the function
 
-    error = None
-    resultado = None
-    try:
-        resultado = f(**kwargs)
-    except Exception as e:
-        error = e.__str__()
+        return: result of the function or str error message if any error occurs
+        """
 
-    return {"data:": resultado, "error": error}
+        error = None
+        resultado = None
+        try:
+            resultado = f(**kwargs)
+        except Exception as e:
+            error = e.__str__()
+
+        return {"data:": resultado, "error": error}
